c82e6abd03d0ff70ecc4935fcb9f2e03b8cf487b
Martin Hochel <hochelmartin@gmail.com>
2019-03-30 12:25:15 +0700

    feat(react-ts-component-patterns): add next lint to demo wrapper

--- react-ts-component-patterns/src/app/demo-wrapper.tsx
+++ react-ts-component-patterns/src/app/demo-wrapper.tsx
@@ -1,14 +1,36 @@
-import React, { ComponentType } from 'react'
+import React, { ComponentType, useEffect, useLayoutEffect } from 'react'
 
-type DemoComponent = ComponentType & { title: string }
+type DemoComponent = ComponentType<{ className?: string }> & { title: string }
 
-export const DemoWrapper = ({ demos }: { demos: DemoComponent[] }) => (
-  <main className="row">
-    {demos.map((Demo, idx) => (
-      <div key={idx} className="paper col col-12 demo-example-margin">
-        <h4>{Demo.title}</h4>
-        <Demo />
-      </div>
-    ))}
-  </main>
-)
+export const DemoWrapper = ({ demos }: { demos: DemoComponent[] }) => {
+
+  return (
+    <main className="row">
+      {demos.map((Demo, idx) => {
+        const NextDemo = demos[idx + 1] ? demos[idx + 1] : demos[0]
+
+        return (
+          <section
+            id={makeValidSelector(Demo.title)}
+            className="demo-example-margin col col-12"
+            key={idx}
+          >
+            <div className="paper">
+              <h4>{Demo.title}</h4>
+              <Demo />
+            </div>
+            <div className="text-right">
+              <a href={`#${makeValidSelector(NextDemo.title)}`} className="">
+                Next 👉
+              </a>
+            </div>
+          </section>
+        )
+      })}
+    </main>
+  )
+}
+
+function makeValidSelector(hash: string) {
+  return hash.replace(/[\s/\\]/g, '-').replace('#', '')
+}
