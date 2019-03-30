071332cbe37a84b05e43ee21783ad57e201255aa
Martin Hochel <hochelmartin@gmail.com>
2019-03-31 00:48:41 +0700

    feat(react-ts-component-patterns): add new custom hook example

--- react-ts-component-patterns/src/app/hooks/finish/app.tsx
+++ react-ts-component-patterns/src/app/hooks/finish/app.tsx
@@ -7,5 +7,6 @@ import { Example as E2 } from './02-stateless-stateful'
 import { Example as E3 } from './03-controlled-uncontrolled'
 import { Example as E4 } from './04-render-prop'
 import { Example as E5 } from './05-hoc'
+import { Example as E6 } from './07-hooks'
 
-export const LiveApp = () => <DemoWrapper demos={[E1, E2, E3, E4, E5]} />
+export const LiveApp = () => <DemoWrapper demos={[E1, E2, E3, E4, E5, E6]} />
