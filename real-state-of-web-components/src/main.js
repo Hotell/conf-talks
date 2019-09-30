099bcb75ce048b8d9eaf11f08fad325a80d14154
Martin Hochel <hochelmartin@gmail.com>
2019-09-30 21:37:35 +0200

    fixup! feat(rs-wc): add initial demo

--- real-state-of-web-components/src/main.js
+++ real-state-of-web-components/src/main.js
@@ -1,10 +1,12 @@
 import { registerElement } from './web-components/finish/counter.component'
+// import './web-components/start/counter.component'
 
 import { bootstrapReactApp } from './app/bootstrap'
 
 const bootstrap = () => {
+  // 🚨comment out before start:
+  // ===========================
   registerElement()
-
   bootstrapReactApp()
 }
 
