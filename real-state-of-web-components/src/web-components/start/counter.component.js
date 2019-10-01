95eae728fb27cc2e01fa924bf0721429d9dd3da8
Martin Hochel <hochelmartin@gmail.com>
2019-10-01 13:32:04 +0200

    fixup! feat(rs-wc): add initial demo

--- real-state-of-web-components/src/web-components/start/counter.component.js
+++ real-state-of-web-components/src/web-components/start/counter.component.js
@@ -1,1 +1,33 @@
 /** @typedef {{incBtn: HTMLButtonElement, decBtn: HTMLButtonElement, countView: HTMLElement}} ViewRef */
+
+class Counter extends HTMLElement {
+  constructor() {
+    super()
+
+    // this.appendChild(template.content.cloneNode(true))
+
+    // this.viewRef = /** @type {ViewRef} */ ({
+    //   countView: this.querySelector('.count'),
+    //   incBtn: this.querySelector('.inc'),
+    //   decBtn: this.querySelector('.dec'),
+    // })
+
+    // this.viewRef.incBtn.addEventListener('click', () => {
+    //   console.log('inc')
+    // })
+    // this.viewRef.decBtn.addEventListener('click', () => {
+    //   console.log('dec')
+    // })
+
+    // RENDER!
+    // this.viewRef.countView.textContent = '0'
+  }
+
+  connectedCallback() {
+    console.log('Mounted')
+  }
+
+  disconnectedCallback() {
+    console.log('Unmounted')
+  }
+}
